21ef858b77e8f656bc9b2e43efc61b524ed68155
Sabrina Sides <sabrinasides91@gmail.com>
2021-05-14 15:54:19 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config() //allows use of process.env with variables from .env file
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const movies = require('./movies.json') //movie data
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(400).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
+//handler function
+function handleGetMovie(req, res){
+    const { genre, country, avg_vote } = req.query;
+    let response = movies;
+
+    //filter for genre by query parameter
+    if(genre){
+        response = response.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase())
+        )}
+
+    //filter for country by query parameter
+    if(country){
+        response = response.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+
+    //filter for avg_vote greater than or equal to query parameter
+    if(avg_vote){
+        const vote = parseFloat(avg_vote)
+        response = response.filter(movie => movie.avg_vote >= vote)
+    }
+
+    res.json(response)
+}
+
+app.get('/movie', handleGetMovie)
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
